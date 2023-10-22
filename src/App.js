a5390b26cb8848722bc239fe3f656c696bb0c3b4
unknown <siddharthdhumal210@gmail.com>
2023-10-22 23:53:05 +0530

    card-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Card from "./Components/Card/Card";
 import Hero from "./Components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Card />
     </div>
   );
 }
--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import styles from "./Card.module.css";
+import RectangleImg from "../../assets/Rectangle 2139.svg";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    // <Tooltip title={`${songs.length} songs`} placement="top" arrow>
+    <div className={styles.wrapper}>
+      <div className={styles.card}>
+        <img src={RectangleImg} alt="album" />
+        <div className={styles.banner}>
+          {/* <Chip
+            label={`${follows} Follows`}
+            size="small"
+            className={styles.chip}
+          /> */}
+          <p className={styles.para}>100M Follows</p>
+        </div>
+      </div>
+      <div className={styles.titleWrapper}>
+        <p>New English Songs</p>
+      </div>
+    </div>
+    // </Tooltip>
+  );
 };
 
 export default Card;
